a56b0c5d38d7cab46bc831739db633381893bc3a
bigbear <bigbear>
2025-10-01 22:20:08 +0800

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello 小燕子</p>
+        <p>hello </p>
         <a
           className="App-link"
           href="https://reactjs.org"
