a3c432fc5e79f52cb2ec80b1d609f1f43ea99fcb
bigbear <bigbear>
2025-10-02 11:54:57 +0800

    feat: update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello </p>
+        <p>hello nn 同学</p>
         <a
           className="App-link"
           href="https://reactjs.org"
